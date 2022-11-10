4f4f69a814bbc66a4b7b5d0a899badd3c8211fec
Shigeyuki Fukushima <shigeyf@liveinstantly.com>
2022-11-10 20:59:56 +0900

    Updated test.ts.

--- src/server/api/test.ts
+++ src/server/api/test.ts
@@ -1,5 +1,12 @@
-export default defineEventHandler((event) => {
-    return {
-        api: "works",
-    }
+import { send } from "h3";
+
+export default defineEventHandler(async (event) => {
+    const id = "theo-0001";
+
+    const blob: Blob = await $fetch(`/contents/${id}.json`, {
+        method: "get",
+        responseType: "blob"
+    });
+
+    await send(event, Buffer.from(Buffer.from(await blob.arrayBuffer())), blob.type);
 });
