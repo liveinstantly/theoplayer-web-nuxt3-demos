e3f5d666ceaf6ee5d6bbd4512c947beeb6ec1909
Shigeyuki Fukushima <shigeyf@liveinstantly.com>
2022-11-10 20:17:17 +0900

    Added some test codes for Netlify Functions.

--- src/server/api/contents.get.ts
+++ src/server/api/contents.get.ts
@@ -4,14 +4,15 @@ import path from "path";
 const contentJsonBase = path.resolve(process.cwd() + "/src/content/player/demos/contents");
 
 export default defineEventHandler((event) => {
+    const json = path.resolve(contentJsonBase + ".json");
     try {
-        const json = path.resolve(contentJsonBase + ".json");
         const contents = JSON.parse(fs.readFileSync(json, 'utf-8'));
         return contents;
     } catch {
         throw createError({
             statusCode: 404,
-            statusMessage: "The specified resource is not found."
+            statusMessage: `The specified resource is not found: ${json}.`,
+            stack: `${JSON.stringify(process.env)}`,
         });
     }
 });
--- src/server/api/contents/[id].get.ts
+++ src/server/api/contents/[id].get.ts
@@ -4,15 +4,16 @@ import path from "path";
 const contentJsonBase = path.resolve(process.cwd() + "/src/content/player/demos/contents");
 
 export default defineEventHandler((event) => {
+    const id = event.context.params.id;
+    const json = path.resolve(contentJsonBase + "/" + id + ".json");
     try {
-        const id = event.context.params.id;
-        const json = path.resolve(contentJsonBase + "/" + id + ".json");
         const contents = JSON.parse(fs.readFileSync(json, 'utf-8'));
         return contents;
     } catch {
         throw createError({
             statusCode: 404,
-            statusMessage: "The specified resource is not found."
+            statusMessage: `The specified resource is not found: ${json}`,
+            stack: `${JSON.stringify(process.env)}`,
         });
     }
 });
